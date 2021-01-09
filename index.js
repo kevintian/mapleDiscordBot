4a21ee7a2f7747c2af92614b1048050c7c15df65
kevintian <kevintian@gatech.edu>
2021-01-09 15:39:52 -0500

    working bot

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
@@ -11,4 +13,4 @@ client.on("message", message => {
   }
 });
 
-client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);
