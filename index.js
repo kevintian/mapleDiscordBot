f4d686b5cb2d11aad3d22c93343cbc3068ae57a8
kevintian <kevintian@gatech.edu>
2021-01-18 01:36:06 -0500

    add flag pings

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-require("dotenv").config();
-
+const dotenv = require("dotenv");
+dotenv.config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json");
+var cron = require("node-cron");
+
+const { prefix, flagTimes } = config;
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", message => {
-  if (message.content === "ping") {
-    message.reply("pong");
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  } else if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
   }
+});
+
+flagTimes.forEach(cronTime => {
+  cron.schedule(
+    cronTime,
+    async () => {
+      /*
+        "asia": "505667339614814230",
+        "botServer": "771601695728730112"
+      */
+      const flagChannel = await guild.channels.fetch("784700229231575040");
+      flagChannel.send("<@&787007601841274920> in five minutes everyone!");
+    },
+    { timezone: "Etc/UTC" }
+  );
 });
 
 client.login(process.env.BOT_TOKEN);
